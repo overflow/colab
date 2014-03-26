d01c59e8af2806570d105d5ce317191fbeef83e8
Orlando Fiol <fiolorlando@gmail.com>
2014-03-26 09:19:13 -0430

    Created data factories with factoryboy for tests

--- src/super_archives/tests.py
+++ src/super_archives/tests.py
@@ -1,16 +1,55 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+import factory
+from factory import fuzzy
+from django.contrib.auth.models import User
+import datetime
+from django.utils import timezone 
 from django.test import TestCase
 
+""" Factory para modelo User """
+class UserFactory(factory.DjangoModelFactory):
+       FACTORY_FOR = User
+       username = factory.Sequence(lambda n: "user_%d" % n)
+       is_active=True
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MailingListFactory(factory.DjangoModelFactory):
+    FACTORY_FOR='super_archives.MailingList'
+    FACTORY_DJANGO_GET_OR_CREATE=('name','email','description','logo',)
+    name=fuzzy.FuzzyText()
+    email=  factory.Sequence(lambda n: "mailist_%d@foo.com" % n)
+    description=fuzzy.FuzzyText()
+    logo=factory.django.FileField(filename='foo.png')
+
+
+class MembershipFactory(factory.DjangoModelFactory):
+    FACTORY_FOR='super_archives.MailingListMembership'
+    FACTORY_DJANGO_GET_OR_CREATE=('user','mailinglist',)
+
+    mailinglist=factory.SubFactory(MailingListFactory)
+    user=factory.SubFactory(UserFactory)
+
+class EmailAddressFactory( factory.DjangoModelFactory):
+    FACTORY_FOR='super_archives.EmailAddress'
+    FACTORY_DJANGO_GET_OR_CREATE=('user','address','realname')
+    user=factory.SubFactory(User)
+    address=fuzzy.FuzzyText()
+    realname=fuzzy.FuzzyText()
+
+class ThreadFactory(factory.DjangoModelFactory):
+    FACTORY_FOR='super_archives.Thread'
+    FACTORY_DJANGO_GET_OR_CREATE=('subject_token','mailinglist',)
+    subject_token=fuzzy.FuzzyText()
+    mailinglist=factory.SubFactory(MailingListFactory)
+
+     
+class MessageFactory(factory.DjangoModelFactory):
+    FACTORY_FOR='super_archives.Message'
+    FACTORY_DJANGO_GET_OR_CREATE=('from_address','thread','subject','subject_clean','body','received_time','message_id')
+    from_address=factory.SubFactory(EmailAddressFactory)
+    thread=factory.SubFactory(ThreadFactory)
+    subject=fuzzy.FuzzyText()
+    subject_clean=fuzzy.FuzzyText()
+    body=fuzzy.FuzzyText()
+    received_time=fuzzy.FuzzyNaiveDateTime(timezone.datetime(timezone.now().year-1,1,1 ))
+    message_id=fuzzy.FuzzyText()
+
+    
